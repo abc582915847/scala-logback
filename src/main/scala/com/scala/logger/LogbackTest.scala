55b5d69debe53709296ea477508ee9a917c3c978
zhangtao <zhangtao@getui.com>
2019-10-23 17:40:36 +0800

    [feature]: add logback for scala demo.

---
+++ src/main/scala/com/scala/logger/LogbackTest.scala
@@ -0,0 +1,20 @@
+package com.scala.logger
+
+import com.typesafe.scalalogging.Logger
+
+object LogbackTest {
+
+  val LOGGER = "scala-logback"
+  lazy val logger = Logger(org.slf4j.LoggerFactory.getLogger(LOGGER))
+
+  def main(args: Array[String]): Unit = {
+
+    println("starting print log >>>")
+    for (a <- 0 to 10) {
+      logger.debug("hello logback debug")
+      logger.info("hello logback info {}", a)
+    }
+
+    println("print log finish <<< ")
+  }
+}
