359e8d481fb501ca5c213dc8c1264238786479ad
zhangtao <zhangtao@getui.com>
2020-02-12 16:16:20 +0800

    [feature]: logback default value.

--- src/main/scala/com/scala/logger/LogbackTest.scala
+++ src/main/scala/com/scala/logger/LogbackTest.scala
@@ -7,7 +7,15 @@ object LogbackTest {
   val LOGGER = "LOGGER-BASIC"
   lazy val logger = Logger(org.slf4j.LoggerFactory.getLogger(LOGGER))
 
+  // logback dynamic file path
+  // vmoptions ==> -Dlogback.home=/Users/Tao/tao/temp/logs2
+  //
   def main(args: Array[String]): Unit = {
+
+//    System.setProperty("logback.home", System.getProperty("logback.home", "logs"))
+
+    // logback default value test ==> value:-defaultValue
+    println(System.getProperty("logback.home"))
 
     println("starting print log >>>")
     for (a <- 0 to 10) {
